840e24891713f5dfb49575ef085716ed5bba074a
Dovineowuor <owuordove@gmail.com>
2023-07-06 10:41:00 +0300

    routing

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+)
+
 from .views import (
     UserViewSet,
     ProductViewSet,
@@ -7,8 +15,10 @@ from .views import (
     OrderViewSet,
     OrderItemViewSet,
     AddressViewSet,
-    PaymentViewSet
+    PaymentViewSet,
 )
+from rest_framework_simplejwt.views import PasswordResetView
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -24,4 +34,10 @@ app_name = 'api'
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('api/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
+
