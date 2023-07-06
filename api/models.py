947cba6f03d60ba96d5662244043dea4df705a8b
Dovineowuor <owuordove@gmail.com>
2023-07-06 09:30:18 +0300

    uuid-models implementation

--- api/models.py
+++ api/models.py
@@ -1,7 +1,10 @@
+import uuid
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
@@ -10,19 +13,23 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.street}, {self.city}, {self.state}"
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=20)
     role = models.CharField(max_length=255)
     addresses = models.ManyToManyField(Address)
+    groups = models.ManyToManyField(Group, related_name='custom_user_set')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',
+        blank=True,
+    )
 
     def __str__(self):
-        return self.name
+        return self.username
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -34,12 +41,14 @@ class Product(models.Model):
         return self.name
 
 class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_date = models.DateField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -49,6 +58,7 @@ class Order(models.Model):
         return f"Order ID: {self.id}"
 
 class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
@@ -58,6 +68,7 @@ class OrderItem(models.Model):
         return f"OrderItem ID: {self.id}"
 
 class Payment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -65,3 +76,4 @@ class Payment(models.Model):
 
     def __str__(self):
         return f"Payment ID: {self.id}"
+
